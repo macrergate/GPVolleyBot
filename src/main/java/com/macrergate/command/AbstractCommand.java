be4246555c453d5a038a60031a35e071f784fa83
Sergey Kosarev <macrergate@gmail.com>
2025-09-02 10:58:12 +0300

    fix displayName on list (+)

--- src/main/java/com/macrergate/command/AbstractCommand.java
+++ src/main/java/com/macrergate/command/AbstractCommand.java
@@ -45,7 +45,7 @@ public abstract class AbstractCommand implements Command {
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
 
-            if (c == '.' || c == '!' || c == '(' || c == ')' || c == '_') {
+            if (c == '.' || c == '!' || c == '(' || c == ')' || c == '_' || c == '+') {
                 builder.append('\\');
             }
 
